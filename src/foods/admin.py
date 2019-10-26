82cfefdf9e1c638a604fc966f42924dfe374070c
Amin <amin@Amins-MacBook-Pro-2.local>
2019-10-26 15:06:07 +0200

    model migration & registered foodproducts to admin

--- src/foods/admin.py
+++ src/foods/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Food
+
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__","description","price"]
+    search_fields = ["title","description","price"]
+    list_filter = ["price"]
+    class Meta:
+        model = Food
+
+admin.site.register(Food, FoodAdmin)
--- src/foods/migrations/0002_auto_20191026_1204.py
+++ src/foods/migrations/0002_auto_20191026_1204.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='food',
             name='description',
-            field=models.TextField(default=b'', null=True),
+            field=models.TextField(default=b'Description will be placed here', null=True),
         ),
         migrations.AddField(
             model_name='food',
