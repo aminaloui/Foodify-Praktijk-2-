3d29c20c175bf7929d7c1254b0b8724ea8c48a3d
Amin <amin@Amins-MacBook-Pro-2.local>
2019-10-26 18:22:23 +0200

    templates for detail / listview

--- src/foodify/urls.py
+++ src/foodify/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^detail/$', 'foods.views.detail_view', name="detail_view"),
+    url(r'^list/$', 'foods.views.list_view', name="list_view"),
 ]
--- src/foods/views.py
+++ src/foods/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 1 item view
+def detail_view(request):
+    print request
+    template = "detail_view.html"
+    context = {}
+    return render(request, template, context)
+#list view
+def list_view(request):
+    print request
+    template = "list_view.html"
+    context = {}
+    return render(request, template, context)
